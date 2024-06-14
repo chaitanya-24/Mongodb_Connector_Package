d8620f14d7d7beb2c85c449ff24a31488dfca322
Chaitanya Sawant <csawant.cs7@gmail.com>
2024-06-15 00:49:09 +0530

    updated entire crud_operation func

--- src/pymongo_connector/mongo_crud.py
+++ src/pymongo_connector/mongo_crud.py
@@ -53,11 +53,11 @@ class mongo_operation:
         self.path=datafile
         
         if self.path.endswith('.csv'):
-            pd.read.csv(self.path,encoding='utf-8')
+            dataframe=pd.read.csv(self.path,encoding='utf-8')
             
         elif self.path.endswith(".xlsx"):
-            dataframe=pd.read_excel(self.path,encoding='utf-8')
+            dataframe=pd.read_excel(self.path)
             
-        datajson=json.loads(dataframe.to_json(orient='record'))
+        datajson=json.loads(dataframe.to_json(orient='records'))
         collection=self.create_collection()
         collection.insert_many(datajson)
